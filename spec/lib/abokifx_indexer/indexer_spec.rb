92a9c995412bd57900dbbb8e0759e7c5855c68b0
Ace Subido <ace.subido@gmail.com>
2017-05-02 14:04:14 +0800

    Add Indexer spec

---
+++ spec/lib/abokifx_indexer/indexer_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+module AbokiFXIndexer
+  describe Indexer, type: %i[virtus] do
+    describe "attributes" do
+      subject { described_class.new }
+      it { is_expected.to have_attribute(:base_url, String) }
+    end
+  end
+
+end
